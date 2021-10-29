3d764c7c2da4065772f8ab4cc291e98b80ddff55
arshakp3 <arshad_cassim@yahoo.com>
2021-10-29 03:18:28 +0200

    Update scripts.js
    new pokemon list created

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = 'RICE';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: "Palkia", height: 4.2, type: ["dragon", "water"], ability: "Telepathy"},
+  {name: "Dragonite", height: 2.2, type: ["dragon", "water"], ability: "Inner-focus, Multiscale"},
+  {name: "Gengar", height: 1.5, type: ["ghost", "poison"], ability: "Levitate"}
+  {name: "Zapdos", height: 1.6, type: ["psychic", "flying"], ability: "Lightningrod"},
+]
