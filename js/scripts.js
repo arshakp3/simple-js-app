1a139185066bbf3260a4d766363a94c5dbb4d50d
arshakp3 <arshad_cassim@yahoo.com>
2021-11-17 23:39:00 +0100

    Replaced  for loop - for each & include  IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,40 @@
 // Array of Pokemons, with their names, heights and types
+//IIFE START
+let pokemonRepository = (function () {
 let pokemonList = [
   {name: "Palkia", height: 4.2, type: ["dragon", "water"], ability: "Telepathy"},
   {name: "Dragonite", height: 2.2, type: ["dragon", "water"], ability: "Inner-focus, Multiscale"},
   {name: "Gengar", height: 1.5, type: ["ghost", "poison"], ability: "Levitate"}
   {name: "Zapdos", height: 1.6, type: ["psychic", "flying"], ability: "Lightningrod"},
 ]
+//returns the pokemonList array
+function getAll() {
+    return pokemonList;
+};
 
+//adds new data to the pokemonList array
+function add(pokemonData) {
+    pokemonList.push(pokemonData);
+};
+
+return {
+  getAll: getAll,
+  add: add
+};
+
+//IIFE END.
+})();
 
 document.write('<ul>')
-for (let i = 0; i < pokemonList.length; i++){
-  document.write('<li>')
-  document.write(pokemonList[i].name + " - Height: " + pokemonList[i].height +"m");
-  if (pokemonList[i].height > 1.7) document.write("  - Wow, thats tall!");
+//define data to new variables using getAll()
+pokemonRepository.getAll().forEach(function(pokemonData){
+  let pokemonName = pokemonData.name
+  let pokemonHeight = pokemonData.height
+  let PokemonType = pokemonData.type
+  //Write to the document.
+  document.write('<li>');
+  document.write(pokemonName + " - Height: " + pokemonHeight +"m");
+  if (pokemonHeight > 1.7) document.write("  - Wow, thats tall!");
   document.write("</li>");
-}
-document.write("</ul>");
+  });
+  document.write("</ul>");
