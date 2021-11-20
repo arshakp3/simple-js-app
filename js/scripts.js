e9a137c36e96ee7aca3cb04804bf3c80943a41d3
arshakp3 <arshad_cassim@yahoo.com>
2021-11-20 02:40:49 +0100

    Button styling, document.write with DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-// Array of Pokemons, with their names, heights and types
-//IIFE START
+/// Array of Pokemons, with their names, heights and types
+///IIFE START
 let pokemonRepository = (function () {
 let pokemonList = [
   {name: "Palkia", height: 4.2, type: ["dragon", "water"], ability: "Telepathy"},
@@ -7,34 +7,59 @@ let pokemonList = [
   {name: "Gengar", height: 1.5, type: ["ghost", "poison"], ability: "Levitate"}
   {name: "Zapdos", height: 1.6, type: ["psychic", "flying"], ability: "Lightningrod"},
 ]
-//returns the pokemonList array
+///returns the pokemonList array
 function getAll() {
     return pokemonList;
 };
 
-//adds new data to the pokemonList array
+///adds new data to the pokemonList array
 function add(pokemonData) {
     pokemonList.push(pokemonData);
 };
 
+/// Creates pokemon list in document
+function addListItem(pokemon) {
+  let pkList = document.querySelector('.pokemon-list');
+  let pkListItem = document.createElement('li');
+  let pkButton = document.createElement('button');
+  pkButton.innerText = pokemon.name;
+  pkButton.classList.add('pk-button');
+  pkListItem.appendChild(pkButton);
+  pkList.appendChild(pkListItem);
+  pkButton.addEventListener('click', function () {
+    showDetails(pokemon);
+  });
+}
+
+function showDetails(pokemon) {
+  console.log(pokemon);
+}
 return {
   getAll: getAll,
-  add: add
+  add: add,
+  addListItem: addListItem
 };
 
-//IIFE END.
+///End of IIFE
 })();
 
-document.write('<ul>')
+pokemonRepository.getAll().forEach(function (pokemon) {
+pokemonRepository.addListItem(pokemon);
+
+});
+
+
+///////////////LAST TASK REF//////////////////////
+/// document.write('<ul>')
 //define data to new variables using getAll()
-pokemonRepository.getAll().forEach(function(pokemonData){
-  let pokemonName = pokemonData.name
-  let pokemonHeight = pokemonData.height
-  let PokemonType = pokemonData.type
+///pokemonRepository.getAll().forEach(function(pokemonData){
+  ////let pokemonName = pokemonData.name
+  ///let pokemonHeight = pokemonData.height
+  ///let PokemonType = pokemonData.type
   //Write to the document.
-  document.write('<li>');
-  document.write(pokemonName + " - Height: " + pokemonHeight +"m");
-  if (pokemonHeight > 1.7) document.write("  - Wow, thats tall!");
-  document.write("</li>");
-  });
-  document.write("</ul>");
+  ///document.write('<li>');
+  ///document.write(pokemonName + " - Height: " + pokemonHeight +"m");
+  ///if (pokemonHeight > 1.7) document.write("  - Wow, thats tall!");
+  ///document.write("</li>");
+  ///});
+  ///document.write("</ul>");
