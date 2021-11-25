0424340375f1dfea2a58048a99ec25bcd9f3906a
arshakp3 <arshad_cassim@yahoo.com>
2021-11-25 23:40:14 +0100

    Modal with pokemon details added

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,7 @@ function addListItem(pokemon) {
 
 function showDetails(pokemon) {
   loadDetails(pokemon).then(function () {
-  console.log(pokemon);
+  showPKModal(pokemon.name, pokemon.height, pokemon.weight, pokemon.types, pokemon.xp, pokemon.imageUrl);
 });
 }
 
@@ -47,32 +47,112 @@ function loadList() {
   console.error(e);
 })
 }
-
 function loadDetails(item) {
 let url = item.detailsURL;
-return fetch(url).then(function (response) {
+  return fetch(url).then(function (response) {
   return response.json();
 }).then(function (details) {
-  item.imageUrl = details.sprites.front_default;
-  item.height = details.height;
-  item.types = details.types;
+    item.imageUrl = details.sprites.front_default;
+    item.height = details.height;
+    item.xp = details.base_experience;
+    item.weight = details.weight;
+    item.types = details.types;
+}).catch(function (e) {
+  console.error(e);
+})
+};
+// Fetches Image, Height, weight, types, base xp.
+function loadDetails(item) {
+  let url = item.detailsURL;
+  return fetch(url).then(function (response) {
+  return response.json();
+}).then(function (details) {
+    item.imageUrl = details.sprites.front_default;
+    item.height = details.height;
+    item.xp = details.base_experience;
+    item.weight = details.weight;
+    item.types = details.types;
 }).catch(function (e) {
   console.error(e);
 });
 }
+
+let pkModalContainer = document.querySelector('#pk-modal-container');
+
+function showPKModal(name, height, weight, types, xp, img) {
+  pkModalContainer.innerHTML = '';
+let pkModal = document.createElement('div');
+  pkModal.classList.add('pkModal');
+//Code for the elements of the modal
+let closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('pk-modal-close');
+  closeButtonElement.innerText = 'Close';
+  closeButtonElement.addEventListener('click', closeModal);
+
+let titleElement = document.createElement('h1');
+  titleElement.innerText = name;
+
+let pkHeight = document.createElement('p');
+  pkHeight.innerText = 'Height: ' + height + 'm';
+
+let pkWeight = document.createElement('p');
+  pkWeight.innerText = 'Weight: ' + weight + 'kg';
+
+let pkTypes = document.createElement('p');
+  let typesString = 'Type(s): ';
+  for (let i = 0; i < types.length; i++) {
+    typesString = typesString + types[i].type.name;
+if (i != types.length - 1) typesString = typesString + ' & ';
+}
+  pkTypes.innerText = typesString;
+
+let pkXP = document.createElement('p');
+  pkXP.innerText = 'Base Experience: ' + xp;
+
+let pkImage = document.createElement('img');
+  pkImage.classList.add('pkimage');
+  pkImage.src = img;
+
+  pkModal.appendChild(titleElement);
+  pkModal.appendChild(closeButtonElement);
+  pkModal.appendChild(pkHeight);
+  pkModal.appendChild(pkWeight);
+  pkModal.appendChild(pkTypes);
+  pkModal.appendChild(pkXP);
+  pkModal.appendChild(pkImage);
+  pkModalContainer.appendChild(pkModal);
+  pkModalContainer.classList.add('is-visible');
+}
+//modal closure
+function closeModal() {
+  pkModalContainer.classList.remove('is-visible');
+};
+
+  pkModalContainer.addEventListener('click', (e) => {
+let target = e.target;
+  if (target === pkModalContainer) {
+  closeModal();
+}
+});
+
+window.addEventListener('keydown', (e) => {
+if (e.key === 'Escape' && pkModalContainer.classList.contains('is-visible')) {
+  closeModal();
+};
+});
+
 return {
   getAll: getAll,
   add: add,
-  addListItem: addListItem
+  addListItem: addListItem,
   loadList: loadList,
   loadDetails: loadDetails
-};
-
-///End of IIFE
+}
+//End of IIFE
 })();
 
-okemonRepository.loadList().then(function() {
-  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.loadList().then(function () {
+    pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
-  });
+});
 });
